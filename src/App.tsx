199c0eb4d18fe93923228b991233e7e597bc5af4
yamagame <yamagame@mac.com>
2022-06-03 07:48:26 +0900

    フォーム値を戻すボタン追加

--- src/App.tsx
+++ src/App.tsx
@@ -103,19 +103,11 @@ const validationSchema = (forcusState: ForcusState) => {
   });
 };
 
-function numberString(val: string) {
-  const zenkaku = "０１２３４５６７８９".split("");
-  const reg = new RegExp("(" + zenkaku.join("|") + ")", "g");
-  const ret = val
-    .replace(reg, (match) => String(zenkaku.indexOf(match)))
-    .replace(/[^0-9]/g, "");
-  return ret;
-}
-
 function App() {
   const [state, dispatch] = React.useReducer(submitReducer, submitInitialState);
   const [count1, setCount1] = React.useState(0);
   const [count2, setCount2] = React.useState(0);
+  const [visibleFlag, setVisibleFlag] = React.useState(true);
   const focusStateRef = React.useRef<ForcusState>({});
 
   const formik = useFormik<FormProps>({
@@ -150,190 +142,206 @@ function App() {
   return (
     <div style={{ margin: 30 }}>
       <div>Hello React</div>
-      <form
-        onSubmit={(e) => {
-          formik.handleSubmit(e);
-        }}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            focusStateRef.current.name = false;
-            formik.handleSubmit();
-          }
-        }}
-      >
-        <div>
-          <PostalCodeInput
-            name="postalCode"
-            placeholder="postal-code"
-            value={formik.values.postalCode}
-            autoComplete="off"
-            onChangeValue={(value: string) => {
-              console.log("$");
-              formik.setFieldValue("postalCode", value);
-            }}
-            onBlur={formik.handleBlur}
-          />
-          <span>{formik.touched.postalCode && formik.errors.postalCode}</span>
-        </div>
-        <div>
-          <DateInput
-            name="date"
-            placeholder="date"
-            value={formik.values.date}
-            autoComplete="off"
-            onChangeValue={(value: string) => {
-              formik.setFieldValue("date", value);
-            }}
-            onBlur={formik.handleBlur}
-          />
-          <span>{formik.touched.date && formik.errors.date}</span>
-        </div>
-        <div>
-          <CreditCardInput
-            name="creditcard"
-            placeholder="credit-card"
-            value={formik.values.creditcard}
-            autoComplete="off"
-            onChangeValue={(value: string) => {
-              formik.setFieldValue("creditcard", value);
-            }}
-            onFocus={() => {
-              focusStateRef.current.creditcard = true;
-            }}
-            onBlur={(e) => {
-              focusStateRef.current.name = false;
-              formik.handleBlur(e);
-              formik.setFieldValue("creditcard", formik.values.creditcard);
-            }}
-          />
-          <span>{formik.touched.creditcard && formik.errors.creditcard}</span>
-        </div>
-        <div style={{ fontSize: 12, margin: 4 }}>Card Bland: {cardbland}</div>
-        <div>
-          <DigitInput
-            name="digit"
-            placeholder="digit"
-            value={formik.values.digit}
-            autoComplete="off"
-            onChangeValue={(value: string) => {
-              formik.setFieldValue("digit", value);
-            }}
-            onBlur={formik.handleBlur}
-          />
-          <span>{formik.touched.digit && formik.errors.digit}</span>
-        </div>
-        <div>
-          <input
-            type="text"
-            placeholder="name"
-            name="name"
-            autoComplete="off"
-            value={formik.values.name}
-            onChange={(e) => {
-              formik.handleChange(e);
-            }}
-            onFocus={() => {
-              focusStateRef.current.name = true;
-            }}
-            onBlur={(e) => {
-              focusStateRef.current.name = false;
-              formik.handleBlur(e);
-              formik.setFieldValue("name", e.target.value.toUpperCase());
-            }}
-          />
-          <span>{formik.touched.name && formik.errors.name}</span>
-        </div>
-        <div>
-          <TextField
-            name="text"
-            placeholder="text"
-            value={formik.values.text}
-            maxLength={16}
-            autoComplete="off"
-            onChange={(e) => {
-              formik.handleChange(e);
-            }}
-            onBlur={(e) => {
-              formik.handleBlur(e);
-              formik.setFieldValue(
-                e.target.name,
-                numberString(e.target.value),
-                true
-              );
-            }}
-          />
-          <span>{formik.touched.text && formik.errors.text}</span>
-        </div>
-        <div>
-          <select
-            name="fruit"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          >
-            <option value="" label="Select a favorite Fruit" />
-            {fruits.map((fruit) => (
-              <option key={fruit.name} value={fruit.name}>
-                {fruit.name}
-              </option>
-            ))}
-          </select>
-          <span>{formik.touched.fruit && formik.errors.fruit}</span>
-        </div>
-        <div>
-          <input
-            type="submit"
-            value="submit"
-            onClick={() => {
-              focusStateRef.current.name = false;
-              focusStateRef.current.creditcard = false;
-            }}
-          />
-        </div>
-      </form>
-      <div style={{ marginTop: 30 }}>
-        <div>
-          <span>postal code: </span>
-          <span>{state.postalCode}</span>
-        </div>
-        <div>
-          <span>date: </span>
-          <span>{state.date}</span>
-        </div>
-        <div>
-          <span>credit card: </span>
-          <span>{state.creditcard}</span>
-        </div>
-        <div>
-          <span>digit: </span>
-          <span>{state.digit}</span>
-        </div>
-        <div>
-          <span>name: </span>
-          <span>{state.name}</span>
-        </div>
-        <div>
-          <span>text: </span>
-          <span>{state.text}</span>
-        </div>
-        <div>
-          <span>fruit: </span>
-          <span>{state.fruit}</span>
-        </div>
-      </div>
-      <div style={{ marginTop: 30 }}>
+      <div>
         <input
-          type="button"
-          value="copy digit to creditcard"
-          onClick={() => {
-            formik.setFieldValue("creditcard", formik.values.digit);
-          }}
+          type="checkbox"
+          checked={visibleFlag}
+          onChange={() => setVisibleFlag(!visibleFlag)}
         />
-        <div />
-        <input type="button" value="count up" onClick={() => countUp()} />
-        <span>
-          {count1}:{count2}
-        </span>
+        表示
       </div>
+      {visibleFlag && (
+        <div>
+          <form
+            onSubmit={(e) => {
+              formik.handleSubmit(e);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                focusStateRef.current.name = false;
+                formik.handleSubmit();
+              }
+            }}
+          >
+            <div>
+              <PostalCodeInput
+                name="postalCode"
+                placeholder="postal-code"
+                value={formik.values.postalCode}
+                autoComplete="off"
+                onChangeValue={(value: string) => {
+                  console.log("$");
+                  formik.setFieldValue("postalCode", value);
+                }}
+                onBlur={formik.handleBlur}
+              />
+              <span>
+                {formik.touched.postalCode && formik.errors.postalCode}
+              </span>
+            </div>
+            <div>
+              <DateInput
+                name="date"
+                placeholder="date"
+                value={formik.values.date}
+                autoComplete="off"
+                onChangeValue={(value: string) => {
+                  formik.setFieldValue("date", value);
+                }}
+                onBlur={formik.handleBlur}
+              />
+              <span>{formik.touched.date && formik.errors.date}</span>
+            </div>
+            <div>
+              <CreditCardInput
+                name="creditcard"
+                placeholder="credit-card"
+                value={formik.values.creditcard}
+                autoComplete="off"
+                onChangeValue={(value: string) => {
+                  formik.setFieldValue("creditcard", value);
+                }}
+                onFocus={() => {
+                  focusStateRef.current.creditcard = true;
+                }}
+                onBlur={(e) => {
+                  focusStateRef.current.name = false;
+                  formik.handleBlur(e);
+                  formik.setFieldValue("creditcard", formik.values.creditcard);
+                }}
+              />
+              <span>
+                {formik.touched.creditcard && formik.errors.creditcard}
+              </span>
+            </div>
+            <div style={{ fontSize: 12, margin: 4 }}>
+              Card Bland: {cardbland}
+            </div>
+            <div>
+              <DigitInput
+                name="digit"
+                placeholder="digit"
+                value={formik.values.digit}
+                autoComplete="off"
+                onChangeValue={(value: string) => {
+                  formik.setFieldValue("digit", value);
+                }}
+                onBlur={formik.handleBlur}
+              />
+              <span>{formik.touched.digit && formik.errors.digit}</span>
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="name"
+                name="name"
+                autoComplete="off"
+                value={formik.values.name}
+                onChange={(e) => {
+                  formik.handleChange(e);
+                }}
+                onFocus={() => {
+                  focusStateRef.current.name = true;
+                }}
+                onBlur={(e) => {
+                  focusStateRef.current.name = false;
+                  formik.handleBlur(e);
+                  formik.setFieldValue("name", e.target.value.toUpperCase());
+                }}
+              />
+              <span>{formik.touched.name && formik.errors.name}</span>
+            </div>
+            <div>
+              <TextField
+                name="text"
+                placeholder="text"
+                value={formik.values.text}
+                maxLength={16}
+                autoComplete="off"
+                onChange={(e) => {
+                  formik.handleChange(e);
+                }}
+                onBlur={(e) => {
+                  formik.handleBlur(e);
+                }}
+              />
+              <span>{formik.touched.text && formik.errors.text}</span>
+            </div>
+            <div>
+              <select
+                name="fruit"
+                value={formik.values.fruit}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              >
+                <option value="" label="Select a favorite Fruit" />
+                {fruits.map((fruit) => (
+                  <option key={fruit.name} value={fruit.name}>
+                    {fruit.name}
+                  </option>
+                ))}
+              </select>
+              <span>{formik.touched.fruit && formik.errors.fruit}</span>
+            </div>
+            <div>
+              <input
+                type="submit"
+                value="submit"
+                onClick={() => {
+                  focusStateRef.current.name = false;
+                  focusStateRef.current.creditcard = false;
+                }}
+              />
+            </div>
+          </form>
+          <div style={{ marginTop: 30 }}>
+            <div>
+              <span>postal code: </span>
+              <span>{state.postalCode}</span>
+            </div>
+            <div>
+              <span>date: </span>
+              <span>{state.date}</span>
+            </div>
+            <div>
+              <span>credit card: </span>
+              <span>{state.creditcard}</span>
+            </div>
+            <div>
+              <span>digit: </span>
+              <span>{state.digit}</span>
+            </div>
+            <div>
+              <span>name: </span>
+              <span>{state.name}</span>
+            </div>
+            <div>
+              <span>text: </span>
+              <span>{state.text}</span>
+            </div>
+            <div>
+              <span>fruit: </span>
+              <span>{state.fruit}</span>
+            </div>
+          </div>
+          <div style={{ marginTop: 30 }}>
+            <input
+              type="button"
+              value="copy value to form"
+              onClick={() => {
+                Object.entries(state).forEach(([key, value]) => {
+                  formik.setFieldValue(key, value);
+                });
+              }}
+            />
+            <div />
+            <input type="button" value="count up" onClick={() => countUp()} />
+            <span>
+              {count1}:{count2}
+            </span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
